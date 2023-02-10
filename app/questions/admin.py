330f97bee9a8589e709369fb9012662c38d54e8f
Pawel Kaminski <pawelkaminski@tipi.software>
2023-02-10 15:22:09 +0100

    feat: add questions admin

--- app/questions/admin.py
+++ app/questions/admin.py
@@ -1,3 +1,24 @@
+from django import forms
 from django.contrib import admin
+from django.db import models
 
-# Register your models here.
+from .models import Answer, Explanation, Question, QuestionSet
+
+admin.site.register(QuestionSet)
+
+
+class AnswerInline(admin.StackedInline):
+    model = Answer
+    formfield_overrides = {
+        models.CharField: {"widget": forms.Textarea(attrs={"rows": 2, "cols": 150})},
+    }
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [AnswerInline]
+    formfield_overrides = {
+        models.CharField: {"widget": forms.Textarea(attrs={"rows": 5, "cols": 150})},
+    }
+
+
+admin.site.register(Question, QuestionAdmin)

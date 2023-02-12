3f374997c7d95a6500ccf74ae910064b7a1cd720
Pawel Kaminski <pawelkaminski@tipi.software>
2023-02-12 15:51:20 +0100

    build: add static file handling

--- app/core/urls.py
+++ app/core/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from markdownx import urls as markdownx
@@ -22,3 +24,8 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("markdownx/", include(markdownx)),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

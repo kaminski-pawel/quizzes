02fba52c29df4c48a2e0ce7630fb717a8d39550b
Pawel Kaminski <pawelkaminski@tipi.software>
2023-02-13 19:01:04 +0100

    feat: extend question serialization with checkboxType and explanation

--- app/questions/serializers.py
+++ app/questions/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Answer, Question
+from .models import Answer, Explanation, Question
 
 
 class AnswerSerializer(serializers.ModelSerializer):
@@ -9,9 +9,16 @@ class AnswerSerializer(serializers.ModelSerializer):
         fields = ["content", "is_correct"]
 
 
+class ExplanationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Explanation
+        fields = ["content"]
+
+
 class QuestionSerializer(serializers.ModelSerializer):
     answers = AnswerSerializer(many=True, read_only=True)
+    explanation = ExplanationSerializer(read_only=True)
 
     class Meta:
         model = Question
-        fields = ["content", "answers"]
+        fields = ["content", "widget", "answers", "explanation"]
